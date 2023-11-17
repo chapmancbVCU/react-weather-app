b77d2154cddcdb19868bc6d56aeda9bb0189434b
Chad Chapman <chad.chapman2010@gmail.com>
2023-11-16 21:24:56 -0500

    Added comments

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,7 +1,14 @@
+/**
+ * @file Sets general layout for the application.
+ * @author Chad Chapman
+ */
 import Navbar from "./Navbar"
 import { Outlet, } from 'react-router-dom';
 
-
+/**
+ * Renders the Navbar and Outlet components.
+ * @returns React.Fragment that contains general layout for the page.
+ */
 export const AppLayout = () => {
     return (
       <>

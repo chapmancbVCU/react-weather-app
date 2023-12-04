153c497cc864cd640740b5f1fa246720ce0f86e1
Chad Chapman <chad.chapman2010@gmail.com>
2023-12-04 18:45:33 -0500

    Began work on search bar, created inputs, and classes for styling

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,30 @@
+import { Form, useLoaderData } from 'react-router-dom';
+import { useEffect } from 'react';
 import '../css/searchbar.css';
 
 function SearchBar() {
 
+    const q = useLoaderData();
+    
     return (
-        <div className="search-bar">Search Bar</div>
+        <div className="search-bar">
+            <Form className="search-form">
+                <input id="q"
+                    type="search"
+                    name="q"
+                    aria-label="Get weather conditions"
+                    placeholder="City or Zip Code">
+                </input>
+                <div 
+                    id="search-spinner"
+                    aria-hidden
+                    hidden={true}
+                />
+                <div className="sr-only" aria-live="polite">
+
+                </div>
+            </Form>
+        </div>
     )
 }
 

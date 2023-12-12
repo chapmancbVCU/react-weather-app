50a0101fe298251981b64f738c04fe1c9c33a4e1
Chad Chapman <chad.chapman2010@gmail.com>
2023-12-12 16:04:58 -0500

    Document formatting

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -5,16 +5,21 @@
 import Navbar from "./Navbar"
 import { Outlet, } from 'react-router-dom';
 import SearchBar from "./SearchBar";
+
+
 /**
  * Renders the Navbar and Outlet components.
  * @returns React.Fragment that contains general layout for the page.
  */
-export const AppLayout = () => {
-    return (
-      <>
-        <Navbar />
-        <SearchBar />
-        <Outlet />
-      </>
-    )
-  }
+function AppLayout() {
+
+  return (
+    <>
+      <Navbar />
+      <SearchBar />
+      <Outlet />
+    </>
+  )
+}
+
+export default AppLayout;

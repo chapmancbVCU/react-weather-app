43e81f3435fb4210681ea3d1eefee02f684968f8
Chad Chapman <chad.chapman2010@gmail.com>
2023-12-11 19:21:44 -0500

    Added constructor and test print statement

--- src/ts/Weather.ts
+++ src/ts/Weather.ts
@@ -2,9 +2,12 @@
  * @class The Weather class is responsible for getting weather data.
  * @author Chad Chapman
  */
-
+import { API } from "./API";
 export class Weather {
+
+    private apiKeys: API;
     constructor() {
-        
+        this.apiKeys = new API();
+        console.log(this.apiKeys.getWeatherKey());
     }
 }

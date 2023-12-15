152c0913115ce07a203113e295ec7685aa66aeef
Chad Chapman <chad.chapman2010@gmail.com>
2023-12-14 20:27:30 -0500

    Removed requestURL from constructor

--- src/ts/Weather.ts
+++ src/ts/Weather.ts
@@ -22,7 +22,7 @@ export class Weather {
      * Creates instance of Weather object.
      */
     constructor() {
-        this.apiKeys = new API("Newport News", 12, 33, "test_url", "simple", 
+        this.apiKeys = new API("Newport News", 12, 33, "simple", 
             "IMPERIAL");
         this.geoLocationInfo = this.getGeoLocationInformation();
         this.city = this.getLocalityInformation(this.geoLocationInfo);

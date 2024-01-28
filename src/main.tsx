13b299abe11346b42ec5a1aa75714ae2ac984f27
Chad Chapman <chad.chapman2010@gmail.com>
2024-01-27 22:54:59 -0500

    Fixed tabs after updating VScode settings

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,8 @@ import Hourly from './routes/Hourly';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { 
-  createBrowserRouter, 
-  RouterProvider
+    createBrowserRouter, 
+    RouterProvider
 } from 'react-router-dom';
 import './css/styles.css';
 import { Weather } from './ts/Weather.ts';
@@ -22,57 +22,58 @@ import { Weather } from './ts/Weather.ts';
 const dateTimeUtility = new DateTimeUtility();
 const weather = new Weather();
 
-// Perform initial query of current location upon start of application.
+/*
+ Perform initial query of current location upon start of application.  Then 
+ get weather data for that initial query.
+ */
 try {
-  const localityInfo = await weather.getCityInfo();
-  console.log(localityInfo);
-  const cityData = await weather.getCityData(localityInfo, "localhost");
-  console.log(cityData)
-  const countryName = await weather.getInitialCountryName();
-  weather.setUnits(countryName);
+    const localityInfo = await weather.getCityInfo();
+    console.log(localityInfo);
+    const cityData = await weather.getCityData(localityInfo, "localhost");
+    console.log(cityData)
+    const countryName = await weather.getInitialCountryName();
+    weather.setUnits(countryName);
 
-  weather.setJSONCityData(cityData);
+    weather.setJSONCityData(cityData);
 
-  // const descriptiveWeatherData = 
-  //   await weather.getWeatherData(weather.getLatitude(), weather.getLongitude(), "localhost");
-  // weather.setJSONDescriptiveWeatherData(descriptiveWeatherData);
-  // console.log(descriptiveWeatherData);
-
-
+    // const descriptiveWeatherData = 
+    //   await weather.getWeatherData(weather.getLatitude(), weather.getLongitude(), "localhost");
+    // weather.setJSONDescriptiveWeatherData(descriptiveWeatherData);
+    // console.log(descriptiveWeatherData);
 } catch (error) {
-  console.log(error)
+    console.log(error)
 }
 
 /**
  * This function is responsible for routing within this application.
  */
 const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/react-weather-app/",
-        element: <Home 
-          dateTimeUtility={dateTimeUtility}
-          weather={weather}
-        />,
-      },
-      {
-        path: "/react-weather-app/daily",
-        element: <Daily 
-          dateTimeUtility={dateTimeUtility}
-          weather={weather}
-        />,
-      },
-      {
-        path: "/react-weather-app/hourly",
-        element: <Hourly 
-          dateTimeUtility={dateTimeUtility}
-          weather={weather}
-        />,
-      },
-    ],
-  },
+    {
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/react-weather-app/",
+                element: <Home 
+                dateTimeUtility={dateTimeUtility}
+                weather={weather}
+                />,
+            },
+                {
+                path: "/react-weather-app/daily",
+                element: <Daily 
+                dateTimeUtility={dateTimeUtility}
+                weather={weather}
+                />,
+            },
+            {
+                path: "/react-weather-app/hourly",
+                element: <Hourly 
+                dateTimeUtility={dateTimeUtility}
+                weather={weather}
+                />,
+            },
+        ],
+    },
 ]);
 
 
@@ -81,7 +82,7 @@ const router = createBrowserRouter([
  * here.
  */
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )

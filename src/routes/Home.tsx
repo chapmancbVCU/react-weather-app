24a3b247b09cb712d2ee3ef0372164a406037546
Chad Chapman <chad.chapman2010@gmail.com>
2024-02-07 21:37:32 -0500

    Added ? after weather variables to supress errors

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -240,9 +240,9 @@ const Home : FC<HomePageProps> = ({ dateTimeUtility, weather }): JSX.Element => 
                             <img className="conditions-icon" src="./icons/weather-windy.png"></img>
                             <div className='current-conditions-info-description'>
                                 Winds
-                                <div>{weather.getWindSpeed(freeTierData.wind.speed)}, {weather.getWindDirection(freeTierData.wind.deg)}</div>
+                                <div>{weather.getWindSpeed(freeTierData?.wind.speed)}, {weather.getWindDirection(freeTierData?.wind.deg)}</div>
                                 Wind Gusts
-                                <div>{weather.getWindSpeed(oneCallData.current.wind_gust)}</div>
+                                <div>{weather.getWindSpeed(oneCallData?.current.wind_gust)}</div>
                             </div>
                         </div>
                     </div>

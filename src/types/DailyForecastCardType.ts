7097695b7f11eeb6a8a3c01af8dbdba3cb0218fe
Chad Chapman <chad.chapman2010@gmail.com>
2024-02-15 19:52:55 -0500

    Created variables for type

---
+++ src/types/DailyForecastCardType.ts
@@ -0,0 +1,10 @@
+/**
+ * @file Defines simplified data for daily forecast card.
+ * @author Chad Chapman
+ */
+
+export type DailyForecastCardType = {
+    description: string
+    icon: string
+    temp: number
+}

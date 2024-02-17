a72f2d13896c3729a2b50f5b2fa5d1af5b989a4d
Chad Chapman <chad.chapman2010@gmail.com>
2024-02-17 08:50:37 -0500

    Refactoring

--- src/types/DailyForecastCardType.ts
+++ src/types/DailyForecastCardType.ts
@@ -4,9 +4,45 @@
  */
 
 export type DailyForecastType = {
+    clouds: number
     description: string
     dt: number
+    dew_point: number
+    feels_like: {
+        day: number
+        eve: number
+        morn: number
+        night: number
+    }
+    humidity: number
     icon: string
     max: number
     min: number
+    moon_phase: number
+    moonrise: number
+    moonset: number
+    pop: number
+    pressure: number
+    rain: number
+    summary: string
+    sunrise: number
+    sunset: number
+    temp: {
+        day: number
+        eve: number
+        max: number
+        min: number
+        morn: number
+        night: number
+    }
+    uvi: number
+    weather: {
+        description: string
+        icon: string
+    }
+    winds: {
+        deg: number
+        gust: number
+        speed: number
+    }
 }

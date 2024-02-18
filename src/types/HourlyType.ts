a69d6c53da80a41a04ebc86f5ea7b1d96c6b36a0
Chad Chapman <chad.chapman2010@gmail.com>
2024-02-17 21:53:28 -0500

    Created variables for type

--- src/types/HourlyType.ts
+++ src/types/HourlyType.ts
@@ -4,5 +4,21 @@
  */
 
 export type HourlyType = {
-    
+    clouds: string
+    dew_point: number
+    dt: number
+    feels_like: number
+    humidity: number
+    pop: number
+    pressure: number
+    temp: number
+    uvi: number
+    visibility: number
+    weather: {
+        description: string
+        icon: string
+    }
+    wind_deg: number
+    wind_gust: number
+    wind_speed: number
 }
